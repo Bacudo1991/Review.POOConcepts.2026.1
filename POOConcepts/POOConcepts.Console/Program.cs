09169384a524df84c76632852502f527e94bbad5
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-01-14 15:03:12 -0500

    Updates

--- POOConcepts/POOConcepts.Console/Program.cs
+++ POOConcepts/POOConcepts.Console/Program.cs
@@ -1,12 +1,18 @@
 ﻿using POOConcepts.Core;
 
-var employee1 = new SalaryEmployee { Id = 1010, Firstname = "Sandra", Lastname = "Lopez", BornDate = new Date(1980, 12, 2),HireDate = new Date(2023, 2, 12), IsActive = true, Salary = 2000000 };
-
-Console.WriteLine(employee1);
 
 
+try
+{
+    var employee1 = new SalaryEmployee { Id = 1010, Firstname = "Sandra", Lastname = "Lopez", BornDate = new Date(1980, 12, 2), HireDate = new Date(2023, 2, 12), IsActive = true, Salary = 2000000 };
 
+    Console.WriteLine(employee1);
+}
+catch (Exception ex)
+{
 
+	Console.WriteLine($"Error: {ex.Message}");
+}
 
 
 
